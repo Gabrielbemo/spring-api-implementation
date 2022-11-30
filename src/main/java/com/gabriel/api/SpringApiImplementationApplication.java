ade05ca2ec308cca0fe620f2ae25b544e2bfed5c
Gabriel Bernardes de Moura <gabrielbemoura@gmail.com>
2022-11-29 22:24:41 -0300

    first commit

---
+++ src/main/java/com/gabriel/api/SpringApiImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiImplementationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiImplementationApplication.class, args);
+    }
+
+}
