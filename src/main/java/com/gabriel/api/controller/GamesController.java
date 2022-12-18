1aada91be841872d41f368ea08728b0257af900b
Gabrielbemo <gabrielbemoura@example.com>
2022-12-17 23:03:00 -0300

    adicionado post

--- src/main/java/com/gabriel/api/controller/GamesController.java
+++ src/main/java/com/gabriel/api/controller/GamesController.java
@@ -7,10 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -32,6 +29,11 @@ public class GamesController {
     @GetMapping(path = "/{id}")
     public ResponseEntity<Game> findById(@PathVariable long id){
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return new ResponseEntity<>(gameService.findById(id), HttpStatus.OK);
+        return ResponseEntity.ok(gameService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Game> save(@RequestBody Game game){
+        return new ResponseEntity<>(gameService.save(game), HttpStatus.CREATED);
     }
 }
--- src/main/java/com/gabriel/api/service/GameService.java
+++ src/main/java/com/gabriel/api/service/GameService.java
@@ -5,11 +5,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class GameService {
-    private List<Game> games = List.of(new Game(1L, "tf2"), new Game(2L, "minecraft"));
+    private static List<Game> games;
+
+    static {
+        games = new ArrayList<>(List.of(new Game(1L, "tf2"), new Game(2L, "minecraft")));
+    }
 
     public List<Game> listAll(){
         return games;
@@ -20,5 +26,11 @@ public class GameService {
                 .filter(game -> game.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Game not found"));
+    }
+
+    public Game save(Game game) {
+        game.setId(ThreadLocalRandom.current().nextLong(3, 10000));
+        games.add(game);
+        return game;
     }
 }
