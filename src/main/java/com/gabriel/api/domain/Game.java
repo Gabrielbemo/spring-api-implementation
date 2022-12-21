15051d60b77e0c93b78792f71e40dd08a168b31f
Gabriel Bernardes de Moura <gabrielbemoura@gmail.com>
2022-12-21 12:25:08 -0300

    configurando banco de dados

--- src/main/java/com/gabriel/api/domain/Game.java
+++ src/main/java/com/gabriel/api/domain/Game.java
@@ -1,11 +1,21 @@
 package com.gabriel.api.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
